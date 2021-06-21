38834af2b8b8e142c8b594f687b22688165ebc99
Seçkin Özülkü <seckinozulku123@gmail.com>
2021-06-21 21:35:37 +0300

    'edit'

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-btn.onclick = function(){
- document.getElementById("nameSurname").innerHTML = "Seçkin Özülkü";
- document.getElementById("age").innerHTML = "26";
- document.getElementById("btn").style.display ="none";
+const myNameSurname = "Seçkin Özülkü";
+const myAge = "26"
+
+btn.onclick = function () {
+    document.getElementById("nameSurname").innerHTML = myNameSurname;
+    document.getElementById("age").innerHTML = myAge;
+    document.getElementById("btn").style.display = "none";
 }
 document.getElementById("linkedinUrl").setAttribute("href", "https://www.linkedin.com/in/seckinozulku/");
 document.getElementById("githubUrl").setAttribute("href", "https://github.com/seckinozulku/");
