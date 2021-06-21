77059a5e18f19999a161809bff40583944574989
Seçkin Özülkü <seckinozulku123@gmail.com>
2021-06-21 16:44:04 +0300

    'ilk odev'

---
+++ index.js
@@ -0,0 +1,8 @@
+btn.onclick = function(){
+ document.getElementById("nameSurname").innerHTML = "Seçkin Özülkü"
+ document.getElementById("age").innerHTML = "26"
+}
+document.getElementById("linkedinUrl").setAttribute("href", "https://www.linkedin.com/in/seckinozulku/");
+document.getElementById("githubUrl").setAttribute("href", "https://github.com/seckinozulku/");
+
+
