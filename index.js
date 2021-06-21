0e065180c7ee5cf1eee85eb4e1b9778d5528b926
Seçkin Özülkü <seckinozulku123@gmail.com>
2021-06-21 18:11:13 +0300

    'edit'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 btn.onclick = function(){
- document.getElementById("nameSurname").innerHTML = "Seçkin Özülkü"
- document.getElementById("age").innerHTML = "26"
+ document.getElementById("nameSurname").innerHTML = "Seçkin Özülkü";
+ document.getElementById("age").innerHTML = "26";
+ document.getElementById("btn").style.display ="none";
 }
 document.getElementById("linkedinUrl").setAttribute("href", "https://www.linkedin.com/in/seckinozulku/");
 document.getElementById("githubUrl").setAttribute("href", "https://github.com/seckinozulku/");
